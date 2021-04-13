599fb79ee7532d8b3c17adce23ba873635d8f74c
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-04-13 12:23:40 -0500

    bootstrap card component for each room render, style based on status

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,9 +1,32 @@
+import { Card } from "react-bootstrap";
+
 export default function Room({ room }) {
+  function statusBg({ status }) {
+    if (status === "Dirty Occupied" || status === "Dirty Vacant") {
+      return "danger";
+    } else if (status === "Clean Occupied" || status === "Clean Vacant") {
+      return "success";
+    } else if (status === "Due for Check-Out") {
+      return "warning";
+    } else if (status === "DND") {
+      return "primary";
+    } else if (status === "OOO") {
+      return "secondary";
+    } else {
+      return "light";
+    }
+  }
+
   return (
     <div>
-      <div>
-        {room.number} | {room.floor} | {room.status} | {room.room_type}
-      </div>
+      <Card bg={statusBg(room)}>
+        <Card.Body>
+          <Card.Title>
+            Room {room.number} - {room.room_type}
+          </Card.Title>
+          <Card.Text>{room.status}</Card.Text>
+        </Card.Body>
+      </Card>
     </div>
-  )
+  );
 }
--- src/components/RoomsList.js
+++ src/components/RoomsList.js
@@ -1,6 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import Room from './Room';
 import axios from 'axios';
+import styled from 'styled-components'
+
+const RoomsListStyles = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 60px;
+`;
+
 
 const RoomsList = props => {
   useEffect(() => {
@@ -11,14 +19,13 @@ const RoomsList = props => {
   const [rooms, setRooms] = useState([]);
 
   return (
-    <div>
-      <h1>Rooms:</h1>
+    <RoomsListStyles>
       <div className="rooms-list">
         {rooms.map((room) => (
           <Room key={room.id} room={room} />
         ))}
       </div>
-    </div>
+    </RoomsListStyles>
   )
 };
 
